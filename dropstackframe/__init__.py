d68d6344bd3339cd49dca612b9c63826254f122c
Jesper Nielsen <jespernielsen1982@gmail.com>
2023-09-02 08:12:01 +0200

    Add a __version__.

--- dropstackframe/__init__.py
+++ dropstackframe/__init__.py
@@ -5,9 +5,12 @@ from dropstackframe.dropstackframe import (
     set_enable_drop_stack_frame,
 )
 
+__version__ = "0.1.0"
+
 __all__ = [
     "DROP_STACK_FRAME_SUPPORTED",
+    "__version__",
     "drop_stack_frame",
-    "set_enable_drop_stack_frame",
     "get_enable_drop_stack_frame",
+    "set_enable_drop_stack_frame",
 ]
